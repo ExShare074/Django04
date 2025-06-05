254e5c3c9c53d4d7f8be6b3fd6704475c0869d5e
Mihail <168080573+ExShare074@users.noreply.github.com>
2025-06-05 23:51:08 +0300

    копирование project_settings/urls.py из Django03

---
+++ project_settings/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from news_app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    path('news/', views.home, name='news_page'),
+    path('news_app/', include('news_app.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
